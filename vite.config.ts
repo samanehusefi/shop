493b4a8ccfc339759a34ed173c3feae8bcde7a2c
samanehUsefi <samanehyousefi64@gmail.com>
2026-02-08 19:37:33 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+        base: "/shop-ts/",
     resolve: {
         alias: {
             "@": path.resolve(process.cwd(), "src"),
